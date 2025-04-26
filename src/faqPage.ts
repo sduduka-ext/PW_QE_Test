0fedec6d10ebb724102bc6f58b6be0645751a3d1
sduduka-ext <sduduka-ext@beachbody.com>
2025-04-26 10:19:45 -0700

    commit changes for faqpage1

---
+++ src/faqPage.ts
@@ -0,0 +1,92 @@
+import { Page } from '@playwright/test';
+
+export class faqPage {
+
+    public cookieButton;
+    public searchInput;
+    public openSourceButton;
+    public uxUiButton;
+    public golangLink;
+    public valesLink;
+    public uxAppLink;
+    public streamLink;
+    public shopLink;
+    public becomeAnAffiliateLink;
+    public manageNutritionSubscriptionLink;
+    public manageDigitalMembershipLink;
+    public orderStatusLink;
+    constructor(private page: Page) {
+        this.cookieButton = this.page.getByRole('button', { name: 'Accept All Cookies' });
+        this.searchInput = this.page.locator('input[id="resources-input"]');
+        this.streamLink = this.page.getByRole('link', { name: 'Stream' });
+        this.shopLink = this.page.getByRole('link', { name: 'Shop' });
+        this.becomeAnAffiliateLink = this.page.getByRole('link', { name: 'Become an Affiliate' });
+        this.manageNutritionSubscriptionLink = this.page.getByRole('link', { name: 'Manage Nutrition Subscription' });
+        this.manageDigitalMembershipLink = this.page.getByRole('link', { name: 'Manage Digital Membership' });
+        this.orderStatusLink = this.page.getByRole('link', { name: 'Order Status' });
+        this.openSourceButton = this.page.getByRole('button', { name: 'Open source' });
+        this.uxUiButton = this.page.getByRole('button', { name: 'UX & UI' });
+        this.golangLink = this.page.locator('a[href="https://resources.codilime.com/golang-checklist/"]');
+        this.valesLink = this.page.locator('a[href="https://codisec.com/veles/"]');
+        this.uxAppLink = this.page.locator('a[href="https://events.codilime.com/ux-in-network-applications"]');
+    }
+
+    async navigateURL(url: string) {
+        await this.page.goto(url);
+    }
+
+    async acceptCookies() {
+        await this.cookieButton.click();
+    }
+    async navigateGoBack() {
+        await this.page.goBack();
+    }
+    async clickLinkStream() {
+        await this.streamLink.click();
+    }
+    async clickLinkShop() {
+        await this.shopLink.click();
+    }
+    async clickLinkBecomeAnAffiliate() {
+        await this.becomeAnAffiliateLink.click();
+    }
+    async clickLinkManageDigitalMembership() {
+        await this.manageDigitalMembershipLink.click();
+    }
+    async clickLinkManageNutritionSubscription() {
+        await this.manageNutritionSubscriptionLink.click();
+    }
+    async clickLinkOrderStatus() {
+        await this.orderStatusLink.click();
+    }
+    async fillSearchInput(text: string) {
+        await this.searchInput.fill(text);
+    }
+    async getAllLinks() {
+        const linksRef = await this.page.locator('a').evaluateAll(evalHref => evalHref.map(eh => eh.getAttribute('href')));
+        console.log("Links hrefs are >>> " + linksRef.length);
+        for (let i = 0; i < linksRef.length; i++) {
+            console.log(i + " > Links hrefs are >>> " + linksRef[i]);
+        }
+    }
+
+    async clickGolangLink() {
+        await this.golangLink.click();
+    }
+
+    async clickVelesLink() {
+        await this.valesLink.click();
+    }
+
+    async clickUxAppLink() {
+        await this.uxAppLink.click();
+    }
+
+    async clickOpenSourceButton() {
+    await this.openSourceButton.click();
+    }
+
+    async clickUxuiButton() {
+    await this.uxUiButton.click();
+    }
+}
--- tests/qetest/test-url-qe.spec.ts
+++ tests/qetest/test-url-qe.spec.ts
@@ -1,62 +1,58 @@
 // Inlcude playwright module
- import { test ,expect} from '@playwright/test';
+import { test, expect } from '@playwright/test';
 //const { test, expect } = require('@playwright/test');
 //const fs = require('fs');
-import {ENV} from "../../utils/env"; // Ensure the file exists at 'c:\QE_PW_Test\tests\utils\env.ts' or update the path accordingly
+import { faqPage } from '../../src/faqPage';
+
+import { ENV } from "../../utils/env"; // Ensure the file exists at 'c:\QE_PW_Test\tests\utils\env.ts' or update the path accordingly
 test.describe('Check URL and its links', async () => {
-  
-    test('Link Shop', async ({ page }) => {
-        console.log(process.env.ENV+" >>>> Navigate - "+process.env.FAQ_URL+" with locale >>>"+process.env.locale);
-        // if(process.env.locale === "en_US")
-        // {
-        //   await page.goto(process.env.FAQ_URL_enUS || 'https://default-url-en-us.com');
-        // }else
-        // {
-          await page.goto(process.env.FAQ_URL || 'https://default-url.com');
-       // }
-        const linksRef=await page.locator('a').evaluateAll(evalHref => evalHref.map(eh=> eh.getAttribute('href')));
-          console.log("Links hrefs are >>> "+linksRef.length);
-          for(let i=0; i<linksRef.length; i++)
-          {
-              console.log(i+" > Links hrefs are >>> "+linksRef[i]);
-          }
-          await page.getByRole('link', { name: 'Shop' }).click();
-      
-      });
-    
-      test('Link Stream', async ({ page }) => {
-        await page.goto('https://stage.faq.bodi.com/');
-        await page.getByRole('link', { name: 'Stream' }).click();
-        //await page.getByTestId('unif_topNav_link_programs').click();
-        await page.goBack()
-      });
-      test('Link Become an Affiliate', async ({ page }) => {
-        await page.goto('https://stage.faq.bodi.com/');
-        await page.getByRole('link', { name: 'Become an Affiliate' }).click();
-        await page.locator('div:nth-child(49) > div > div > div > div > div > div > div > div > div:nth-child(2) > div').first().click();
-        await page.getByRole('link', { name: 'Become an Affiliate' }).click();
-      });
-      test('Link Manage Nutrition Subscription', async ({ page }) => {
-        await page.goto('https://stage.faq.bodi.com/');
-        await page.getByRole('link', { name: 'Manage Nutrition Subscription' }).click();
-        await page.getByText('This site can’t be reached').click();
-      });
-      test('Link Manage Digital Membership', async ({ page }) => {
-          await page.goto('https://stage.faq.bodi.com/');
-          await page.getByRole('link', { name: 'Manage Digital Membership' }).click();
-          await page.getByText('This site can’t be reached').click();
-        });
-      test('Order Status', async ({ page }) => {
-        await page.goto('https://stage.faq.bodi.com/');
-        await page.getByRole('link', { name: 'Order Status' }).click();
-      //  await page.goto('chrome-error://chromewebdata/');
-      //   await page.getByText('This page isn’t working').click();
-      //   await page.getByRole('link', { name: 'Return or Exchange' }).click();
-      //   await page.goto('https://stage.faq.bodi.com/');
-      //   await page.getByRole('link', { name: 'Update Account' }).click();
-      //   await page.getByText('This site can’t be reached').click();
-      //   await page.getByRole('link', { name: 'Update Payment Method' }).click();
-      //   await page.getByRole('link', { name: 'Partner Forms' }).click();
-      //   await page.getByRole('link', { name: 'Partner Forms' }).click();
-    });      
+
+  test('Link Shop', async ({ page }) => {
+
+    console.log(process.env.ENV + " >>>> Navigate - " + process.env.FAQ_URL + " with locale >>>" + process.env.locale);
+    const faqPageInstance = new faqPage(page);
+    await faqPageInstance.navigateURL(process.env.FAQ_URL || 'https://default-url.com');
+    //  faqPageInstance.getAllLinks();
+    await faqPageInstance.clickLinkShop();
+    await faqPageInstance.navigateGoBack();
   });
+
+  test('Link Stream', async ({ page }) => {
+    const faqPageInstance = new faqPage(page);
+    faqPageInstance.clickLinkStream();
+    faqPageInstance.navigateGoBack();
+  });
+  test('Link Become an Affiliate', async ({ page }) => {
+    const faqPageInstance = new faqPage(page);
+    faqPageInstance.clickLinkBecomeAnAffiliate();
+    faqPageInstance.navigateGoBack();
+  });
+  test('Link Manage Nutrition Subscription', async ({ page }) => {
+    const faqPageInstance = new faqPage(page);
+    faqPageInstance.clickLinkManageNutritionSubscription();
+
+    await page.getByText('This site can’t be reached').click();
+    faqPageInstance.navigateGoBack();
+  });
+  test('Link Manage Digital Membership', async ({ page }) => {
+    const faqPageInstance = new faqPage(page);
+    faqPageInstance.clickLinkManageDigitalMembership();
+    await page.getByText('This site can’t be reached').click();
+    faqPageInstance.navigateGoBack();
+  });
+  test('Order Status', async ({ page }) => {
+    const faqPageInstance = new faqPage(page);
+    faqPageInstance.clickLinkOrderStatus();
+
+    faqPageInstance.navigateGoBack();
+    //  await page.goto('chrome-error://chromewebdata/');
+    //   await page.getByText('This page isn’t working').click();
+    //   await page.getByRole('link', { name: 'Return or Exchange' }).click();
+    //   await page.goto('https://stage.faq.bodi.com/');
+    //   await page.getByRole('link', { name: 'Update Account' }).click();
+    //   await page.getByText('This site can’t be reached').click();
+    //   await page.getByRole('link', { name: 'Update Payment Method' }).click();
+    //   await page.getByRole('link', { name: 'Partner Forms' }).click();
+    //   await page.getByRole('link', { name: 'Partner Forms' }).click();
+  });
+});
